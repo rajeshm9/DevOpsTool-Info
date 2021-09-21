39c023b9fcc4ee5e62091f1ef9cf4f1981e23916
rajeshm9 <rajeshmahajan09@gmail.com>
2021-09-21 20:46:59 +0530

    Update useful-shell-sc.sh

--- useful-shell-sc.sh
+++ useful-shell-sc.sh
@@ -1,3 +1,6 @@
 # Extact substring using grep
 # ehco key="121"
 grep -oP 'key="\K[^"]+'
+
+# Get all Running pod from particular namespace
+kubectl get pod -n <namespace> --field-selector=status.phase==Running -o jsonpath="{.items[0].metadata.name}
