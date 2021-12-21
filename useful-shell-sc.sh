cb726b94b000e1bed8e6c8d01ed0706846784cf6
rajeshm9 <rajeshmahajan09@gmail.com>
2021-12-21 15:45:45 +0530

    Update useful-shell-sc.sh

--- useful-shell-sc.sh
+++ useful-shell-sc.sh
@@ -4,3 +4,6 @@ grep -oP 'key="\K[^"]+'
 
 # Get all Running pod from particular namespace
 kubectl get pod -n <namespace> --field-selector=status.phase==Running -o jsonpath="{.items[0].metadata.name}
+
+# Failed Pod 
+kubectl get pod -n <namespace> |egrep -v 'Running|Completed'
