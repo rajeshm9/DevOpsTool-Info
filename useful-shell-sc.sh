7012b38d13c4bfeb710eb25df8ce12e7b704bd8a
rajeshm9 <rajeshmahajan09@gmail.com>
2022-04-22 15:36:03 +0530

    Deleting of ns

--- useful-shell-sc.sh
+++ useful-shell-sc.sh
@@ -1,14 +1,14 @@
-# Extact substring using grep
-# ehco key="121"
+############ Extact substring using grep ############
+ehco key="121"
 grep -oP 'key="\K[^"]+'
 
-# Get all Running pod from particular namespace
+############ Get all Running pod from particular namespace ############
 kubectl get pod -n <namespace> --field-selector=status.phase==Running -o jsonpath="{.items[0].metadata.name}
 
-# Failed Pod 
+############ Failed Pod  ############
 kubectl get pod -n <namespace> |egrep -v 'Running|Completed'
 
-# Kubectl api List
+############ Kubectl api List ############
 for apipath in $(kubectl api-versions | sort | sed '/\//{H;1h;$!d;x}'); do
   version=${apipath#*/}
   api=${apipath%$version}
@@ -20,7 +20,7 @@ for apipath in $(kubectl api-versions | sort | sed '/\//{H;1h;$!d;x}'); do
 done
 `"
 
-# Get all Pod logs in one namespace
+############ Get all Pod logs in one namespace #########
 NS=${NS:-${1}}
 
 for pod in $(kubectl get pods -n ${NS} -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
@@ -38,4 +38,11 @@ do
     fi
 done
 
+############ Delete Namespace in Terminating Status #########
+(
+NAMESPACE=test
+kubectl proxy &
+kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
+curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+)
 
